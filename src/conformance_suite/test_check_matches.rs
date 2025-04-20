a61cf605b5a94223334399c51f87520bee7606c7
Sivert Cubedo <sivertsac@gmail.com>
2025-04-20 23:56:55 +0200

    Refactor check matches test to test multiple synthetic load sizes

--- src/conformance_suite/test_check_matches.rs
+++ src/conformance_suite/test_check_matches.rs
@@ -5,20 +5,29 @@ use crate::framework::scanmem_driver;
 use crate::*;
 
 pub fn scenario_func_test_check_matches(reference_scanmem_program: &str, test_scanmem_program: &str, synthetic_load_program: &str, _synthetic_load_random_seed: u64, fixture_index: usize, verbose: bool) -> TestResult {
-    const SYNTHETIC_LOAD_SIZE: usize = 0x1_000_000usize;
 
     // How many threads to use.
-    const FIXTURE_DATA: [u32; 6] = [
+    const THREAD_COUNT_ARRAY: [u32; 6] = [
         1, 2, 3, 11, 20, 32
     ];
-    let nthreads = FIXTURE_DATA[fixture_index];
+
+    // Corresponds to fixture index.
+    const SYNTHETIC_LOAD_SIZE_ARRAY: [usize; 6] = [
+        0x0usize,
+        0x100usize,
+        0x1_000usize,
+        0x10_000usize,
+        0x1_000_000usize,
+        0x10_000_000usize,
+    ];
+    let synthetic_load_size = SYNTHETIC_LOAD_SIZE_ARRAY[fixture_index];
 
     // Create synthetic_load child process and init.
     let mut synthetic_load_process = synthetic_load_driver::SyntheticLoadDriver::create(synthetic_load_program, verbose).unwrap();
     let synthetic_load_process_pid = synthetic_load_process.get_pid();
 
     // Init synthetic_load, fill with 1s.
-    synthetic_load_process.command_set_memory_size(SYNTHETIC_LOAD_SIZE).unwrap();
+    synthetic_load_process.command_set_memory_size(synthetic_load_size).unwrap();
     synthetic_load_process.command_fill(0x1).unwrap();
 
     // To make sure the memory of the target process is the same between scans, manually stop the target process before we attach scanmem processes.
@@ -26,9 +35,15 @@ pub fn scenario_func_test_check_matches(reference_scanmem_program: &str, test_sc
 
     // Run test
 
-    // Create scanmem child processes.
-    let mut reference_scanmem = scanmem_driver::ScanmemDriver::create(reference_scanmem_program, synthetic_load_process_pid, nthreads, verbose).unwrap();
-    let mut test_scanmem = scanmem_driver::ScanmemDriver::create(test_scanmem_program, synthetic_load_process_pid, nthreads, verbose).unwrap();
+    // Create referebce scanmem child processe, we only need one since we assume it is correct.
+    let mut reference_scanmem = scanmem_driver::ScanmemDriver::create(reference_scanmem_program, synthetic_load_process_pid, 0, verbose).unwrap();
+
+    // Create test scanmem processes
+    let mut test_scanmem_list = vec![];
+    for i in 0..THREAD_COUNT_ARRAY.len() {
+        let test_scanmem = scanmem_driver::ScanmemDriver::create(test_scanmem_program, synthetic_load_process_pid, THREAD_COUNT_ARRAY[i], verbose).unwrap();
+        test_scanmem_list.push(test_scanmem);
+    }
     let mut test_result = TestResult::Pass;
 
     {
@@ -36,14 +51,18 @@ pub fn scenario_func_test_check_matches(reference_scanmem_program: &str, test_sc
         // We can't attach 2 times to the same process at the same time, so we need to make sure we're done scanning before attaching the other scanmem process.
         reference_scanmem.write_line_stdin("= 1").unwrap();
         let reference_match_data: MatchData = reference_scanmem.read_match_data();
-        test_scanmem.write_line_stdin("= 1").unwrap();
-        let test_match_data: MatchData = test_scanmem.read_match_data();
-        // Validate first search regions even though we're not testing this explicitly.
-        expect_eq_r!(test_result, reference_match_data.error, false);
-        expect_eq_r!(test_result, test_match_data.error, false);
-        expect_ge_r!(test_result, reference_match_data.match_count, SYNTHETIC_LOAD_SIZE as u64);
-        expect_ge_r!(test_result, test_match_data.match_count, SYNTHETIC_LOAD_SIZE as u64);
-        expect_eq_r!(test_result, reference_match_data.match_count, test_match_data.match_count);
+
+        for i in 0..THREAD_COUNT_ARRAY.len() {
+            let test_scanmem = &mut test_scanmem_list[i];
+            test_scanmem.write_line_stdin("= 1").unwrap();
+            let test_match_data: MatchData = test_scanmem.read_match_data();
+            // Validate first search regions even though we're not testing this explicitly.
+            expect_eq_r!(test_result, reference_match_data.error, false, format!("nthreads {} failed", THREAD_COUNT_ARRAY[i]));
+            expect_eq_r!(test_result, test_match_data.error, false, format!("nthreads {} failed", THREAD_COUNT_ARRAY[i]));
+            expect_ge_r!(test_result, reference_match_data.match_count, synthetic_load_size as u64, format!("nthreads {} failed", THREAD_COUNT_ARRAY[i]));
+            expect_ge_r!(test_result, test_match_data.match_count, synthetic_load_size as u64, format!("nthreads {} failed", THREAD_COUNT_ARRAY[i]));
+            expect_eq_r!(test_result, reference_match_data.match_count, test_match_data.match_count, format!("nthreads {} failed", THREAD_COUNT_ARRAY[i]));
+        }
     }
 
     // Mofify synthetic load to contain 2s.
@@ -56,28 +75,36 @@ pub fn scenario_func_test_check_matches(reference_scanmem_program: &str, test_sc
         // We can't attach 2 times to the same process at the same time, so we need to make sure we're done scanning before attaching the other scanmem process. 
         reference_scanmem.write_line_stdin("= 2").unwrap();
         let reference_match_data: MatchData = reference_scanmem.read_match_data();
-        test_scanmem.write_line_stdin("= 2").unwrap();
-        let test_match_data: MatchData = test_scanmem.read_match_data();
-        // Validate first search regions even though we're not testing this explicitly.
-        expect_eq_r!(test_result, reference_match_data.error, false);
-        expect_eq_r!(test_result, test_match_data.error, false);
-        expect_ge_r!(test_result, reference_match_data.match_count, SYNTHETIC_LOAD_SIZE as u64);
-        expect_ge_r!(test_result, test_match_data.match_count, SYNTHETIC_LOAD_SIZE as u64);
-        expect_eq_r!(test_result, reference_match_data.match_count, test_match_data.match_count);
+
+        for i in 0..THREAD_COUNT_ARRAY.len() {
+            let test_scanmem = &mut test_scanmem_list[i];
+            test_scanmem.write_line_stdin("= 2").unwrap();
+            let test_match_data: MatchData = test_scanmem.read_match_data();
+            // Validate first search regions even though we're not testing this explicitly.
+            expect_eq_r!(test_result, reference_match_data.error, false, format!("nthreads {} failed", THREAD_COUNT_ARRAY[i]));
+            expect_eq_r!(test_result, test_match_data.error, false, format!("nthreads {} failed", THREAD_COUNT_ARRAY[i]));
+            expect_ge_r!(test_result, reference_match_data.match_count, synthetic_load_size as u64, format!("nthreads {} failed", THREAD_COUNT_ARRAY[i]));
+            expect_ge_r!(test_result, test_match_data.match_count, synthetic_load_size as u64, format!("nthreads {} failed", THREAD_COUNT_ARRAY[i]));
+            expect_eq_r!(test_result, reference_match_data.match_count, test_match_data.match_count, format!("nthreads {} failed", THREAD_COUNT_ARRAY[i]));
+        }
     }
 
     // Cleanup.
     reference_scanmem.write_line_stdin("exit").unwrap();
-    test_scanmem.write_line_stdin("exit").unwrap();
+    for test_scanmem in &mut test_scanmem_list {
+        test_scanmem.write_line_stdin("exit").unwrap();
+    }
     let reference_scanmem_exit_status = reference_scanmem.wait().unwrap();
-    let test_scanmem_exit_status = test_scanmem.wait().unwrap();
     if !reference_scanmem_exit_status.success() {
         println!("Error: scanmem did not exit successfully, ExitStatus = {} ({})", reference_scanmem_exit_status.code().unwrap(), reference_scanmem_exit_status);
         test_result = TestResult::Fail;
     }
-    if !test_scanmem_exit_status.success() {
-        println!("Error: scanmem did not exit successfully, ExitStatus = {} ({})", test_scanmem_exit_status.code().unwrap(), test_scanmem_exit_status);
-        test_result = TestResult::Fail;
+    for test_scanmem in &mut test_scanmem_list {
+        let test_scanmem_exit_status = test_scanmem.wait().unwrap();
+        if !test_scanmem_exit_status.success() {
+            println!("Error: scanmem did not exit successfully, ExitStatus = {} ({})", test_scanmem_exit_status.code().unwrap(), test_scanmem_exit_status);
+            test_result = TestResult::Fail;
+        }
     }
 
     // Resume target process such that it can close gracefully.
